8d39418fd6ebee283e9190d27b9d9429a2733960
Shihan Chowdhury <hackedgdown@gmail.com>
2023-05-16 21:08:01 +0100

    72-7 (Interesting) Load Data Based On The Page Number And Size Using Chatgpt done

--- ema-john-server/index.js
+++ ema-john-server/index.js
@@ -40,8 +40,11 @@ async function run() {
         });
 
         app.get('/products', async (req, res) => {
-            console.log(req.query)
-            const result = await productsCollection.find().toArray();
+            // console.log(req.query);
+            const page = parseInt(req.query.page) || 0;
+            const limit = parseInt(req.query.limit) || 10;
+            const skip = page * limit; 
+            const result = await productsCollection.find().skip(skip).limit(limit).toArray();
             res.send(result);
         });
 
